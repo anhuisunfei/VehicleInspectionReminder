cbc6d4e0820e637c514ab07f3d294b328e2551f9
unknown <sunf@sunf.cbichina.com>
2016-03-21 13:46:52 +0800

    test

---
+++ src/VehicleInspectionReminder/VehicleInspectionReminder.Service/BrandService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInspectionReminder.Data.Infrastructure;
+using VehicleInspectionReminder.Data.Repository;
+using VehicleInspectionReminder.Model;
+
+namespace VehicleInspectionReminder.Service
+{
+
+
+	public class BrandService : IBrandService
+	{
+		private readonly IBrandRepository _brandRepository;
+		private readonly IUnitOfWork _unitOfWork;
+		public BrandService(IBrandRepository brandRepository,IUnitOfWork unitOfWork)
+		{
+			_brandRepository = brandRepository;
+			_unitOfWork = unitOfWork;
+		}
+		public void AddBrand(Brand brand)
+		{
+			_brandRepository.Insert(brand);
+			_unitOfWork.Commit();
+		}
+
+		public IEnumerable<Brand> GetAll()
+		{
+			return _brandRepository.GetAll();
+		}
+	}
+
+	public interface IBrandService
+	{
+		void AddBrand(Brand brand);
+
+		IEnumerable<Brand> GetAll();
+	}
+}
