c734822775f48daf2ea0ea4920debf545c2927e8
anhuisunfei <1195255889@qq.com>
2016-03-21 16:31:04 +0800

    update ownerinfo

---
+++ src/VehicleInspectionReminder/VehicleInspectionReminder.Data/Migrations/201603210823475_update Owner info .cs
@@ -0,0 +1,16 @@
+namespace VehicleInspectionReminder.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateOwnerinfo : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ src/VehicleInspectionReminder/VehicleInspectionReminder.Service/OwnerInfoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInspectionReminder.Data.Infrastructure;
+using VehicleInspectionReminder.Data.Repository;
+using VehicleInspectionReminder.Model;
+
+namespace VehicleInspectionReminder.Service
+{
+	public class OwnerInfoService:IOwnerInfoService
+	{
+
+		private readonly IOwnerInfoRepository _ownerInfoRepository;
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OwnerInfoService(IOwnerInfoRepository ownerInfoRepository, IUnitOfWork unitOfWork)
+		{
+			_ownerInfoRepository = ownerInfoRepository;
+			_unitOfWork = unitOfWork;
+		}
+		public void AddOwnerInfo(OwnerInfo model)
+		{
+			 _ownerInfoRepository.Insert(model);
+			_unitOfWork.Commit();
+		}
+
+		public OwnerInfo GetOwnerInfoByUserId(Guid id)
+		{
+			return _ownerInfoRepository.Get(p => p.AspNetUserId == id);
+		}
+
+
+		public void UpdateOwnerInfo(OwnerInfo model)
+		{
+			_ownerInfoRepository.Update(model);
+			_unitOfWork.Commit();
+		}
+	}
+
+	public interface IOwnerInfoService
+	{
+		void AddOwnerInfo(OwnerInfo model);
+		void UpdateOwnerInfo(OwnerInfo model);
+
+		OwnerInfo GetOwnerInfoByUserId(Guid id);
+	}
+}
