c22e7c814b459cb2347f57e2973f64f75eb50dc8
anhuisunfei <1195255889@qq.com>
2016-03-22 15:18:15 +0800

    remove file

---
+++ src/VehicleInspectionReminder - 副本/VehicleInspectionReminder.Data/Repository/OwnerInfoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInspectionReminder.Data.Infrastructure;
+using VehicleInspectionReminder.Model;
+
+namespace VehicleInspectionReminder.Data.Repository
+{
+	public class OwnerInfoRepository : EfRepositoryBase<OwnerInfo>, IOwnerInfoRepository
+	{
+		public OwnerInfoRepository(IDataBaseFactory factory)
+			: base(factory)
+		{
+
+		}
+	}
+
+	public interface IOwnerInfoRepository : IRepository<OwnerInfo>
+	{
+
+	}
+}
---
+++ src/VehicleInspectionReminder - 副本/VehicleInspectionReminder.Web/Migrations/Configuration.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using VehicleInspectionReminder.Web.Models;
+
+namespace VehicleInspectionReminder.Web.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VehicleInspectionReminder.Web.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VehicleInspectionReminder.Web.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            var passwordHash = new PasswordHasher();
+            string password = passwordHash.HashPassword("sunf");
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "sunf",
+                    PasswordHash = password,
+                    Email = "anhuisunfei@gmail.com",
+                    SecurityStamp = "aaa"
+
+
+                }, new ApplicationUser
+                {
+                    UserName = "baiy",
+                    PasswordHash = password,
+                    Email = "baiy@gmail.com",
+                    SecurityStamp = "bbb"
+                });
+
+            context.Roles.AddOrUpdate(r => r.Name, new AppRole
+            {
+                Name = "Admin" // 管理员
+            }, new AppRole
+            {
+                Name = "CarOwner" // 车主
+            });
+
+
+
+
+        }
+    }
+}
